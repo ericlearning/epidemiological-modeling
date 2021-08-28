8d25ce943ab77037af5ce8a7325035cdce35f037
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-08-29 04:26:12 +0900

    SIRS model added

--- models.py
+++ models.py
@@ -6,11 +6,27 @@ class SIR:
         self.gamma = gamma
         self.N = N
     
-    def ds_dt(self, I, S):
+    def ds_dt(self, S, I, R):
         return -(self.beta * I * S) / self.N
     
-    def di_dt(self, I, S):
+    def di_dt(self, S, I, R):
         return (self.beta * I * S) / self.N - self.gamma * I
     
-    def dr_dt(self, I, S):
+    def dr_dt(self, S, I, R):
         return self.gamma * I
+
+class SIRS:
+    def __init__(self, beta, gamma, tau, N):
+        self.beta = beta
+        self.gamma = gamma
+        self.tau = tau
+        self.N = N
+    
+    def ds_dt(self, S, I, R):
+        return -(self.beta * I * S) / self.N + self.tau * R
+    
+    def di_dt(self, S, I, R):
+        return (self.beta * I * S) / self.N - self.gamma * I
+    
+    def dr_dt(self, S, I, R):
+        return self.gamma * I - self.tau * R
