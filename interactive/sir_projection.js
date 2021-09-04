4bb44f9d7e477bca02cc0b101b0703d61d82302b
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-09-04 18:41:01 +0900

    instance version of code implemented

--- interactive/sir_projection.js
+++ interactive/sir_projection.js
@@ -1,57 +1,65 @@
-// plotting parameters
-let plt;
-let sliders;
-let checkbox;
-let h = 0.1;
-let N = 20000.0;
-let useFixedN = true;
-let colors = [[0, 0, 255], [255, 0, 0], [0, 255, 0]];
+const s = (p) => {
+    // plotting parameters
+    let plt;
+    let sliders;
+    let checkbox;
+    let h = 0.1;
+    let N = 20000.0;
+    let useFixedN = true;
+    let colors = [p.color(0, 0, 255),
+                  p.color(255, 0, 0),
+                  p.color(0, 255, 0)];
 
-// t, solution, and predictions
-let t, gt, rk;
+    // t, solution, and predictions
+    let t, gt, rk;
 
-// ODE of the projection model
-let model = new SIR(0.7, 0.2);
+    // ODE of the projection model
+    let model = new SIR(0.7, 0.2);
 
-// setup the visualizations for the analyses
-function setup() {
-    createCanvas(500, 500);
+    // setup the visualizations for the analyses
+    p.setup = () => {
+        canvas = p.createCanvas(500, 500);
 
-    [sliders, init] = sliderInit([[0, N, N - 2000], [0, N, 2000], [0, N, 0]]);
-    checkbox = checkboxInit(sliders[0], fixedN);
+        [sliders, init] = sliderInit([[0, N, N - 2000], [0, N, 2000], [0, N, 0]], p=p);
+        checkbox = checkboxInit(sliders[0], fixedN, p=p);
 
-    args = [];
-    [t, rk] = run_analysis(model.interact, init, args, h = h);
-    points = generatePointsMulti(t, rk, sliders.length);
-
-    plt = new GPlot(this, 0, 0, width, height);
-    plt.setTitleText("SIR Model");
-    plt.getXAxis().setAxisLabelText("Time (days)");
-    plt.getYAxis().setAxisLabelText("Population");
-    plt.setYLim(-2000.0, 26000.0);
-    addLayers(plt, ["S", "I", "R"], points, colors);
-}
-
-// draw the plot and update them based on slider value
-function draw() {
-    if (JSON.stringify(init) != JSON.stringify(getSliderValues(sliders))) {
-        init = getSliderValues(sliders);
         args = [];
         [t, rk] = run_analysis(model.interact, init, args, h = h);
         points = generatePointsMulti(t, rk, sliders.length);
-        setLayers(plt, ["S", "I", "R"], points);
-    }
 
-    let realN = init.reduce((a, b) => a + b, 0);
-    if (realN != N && useFixedN) {
-        let diff = (N - realN) / init.length;
-        for (let i = 0; i < sliders.length; i++) {
-            sliders[i].value(init[i] + diff);
+        plt = new GPlot(p, 0, 0, p.width, p.height);
+        plt.setTitleText("SIR Model");
+        plt.getXAxis().setAxisLabelText("Time (days)");
+        plt.getYAxis().setAxisLabelText("Population");
+        plt.setYLim(-2000.0, 26000.0);
+        addLayers(plt, ["S", "I", "R"], points, colors);
+    };
+
+    // draw the plot and update them based on slider value
+    p.draw = () => {
+        if (JSON.stringify(init) != JSON.stringify(getSliderValues(sliders))) {
+            init = getSliderValues(sliders);
+            args = [];
+            [t, rk] = run_analysis(model.interact, init, args, h = h);
+            points = generatePointsMulti(t, rk, sliders.length);
+            setLayers(plt, ["S", "I", "R"], points);
         }
-        realN = N;
-    }
 
-    drawPlot(plt);
-    drawUI(sliders, ["Susceptible", "Infected", "Recovered"],
-        checkbox, realN, colors);
+        let realN = init.reduce((a, b) => a + b, 0);
+        if (realN != N && useFixedN) {
+            let diff = (N - realN) / init.length;
+            for (let i = 0; i < sliders.length; i++) {
+                sliders[i].value(init[i] + diff);
+            }
+            realN = N;
+        }
+
+        p.background(255);
+        drawPlot(plt);
+        drawUI(sliders, ["Susceptible", "Infected", "Recovered"],
+            checkbox, realN, colors, p=p);
+    };
+
 }
+
+let vis = new p5(s);
--- interactive/visualization/utils.js
+++ interactive/visualization/utils.js
@@ -1,15 +1,79 @@
-function sliderInit(sliderInit) {
+function sliderInit(sliderInit, p=null) {
     let out = [];
     let initVal = [];
     for (let i = 0; i < sliderInit.length; i++) {
-        let cur_slider = createSlider(sliderInit[i][0],
-            sliderInit[i][1], sliderInit[i][2], 0);
+        let cur_slider;
+        if (p == null){
+            cur_slider = createSlider(sliderInit[i][0],
+                sliderInit[i][1], sliderInit[i][2], 0);
+        }
+        else{
+            cur_slider = p.createSlider(sliderInit[i][0],
+                sliderInit[i][1], sliderInit[i][2], 0);
+        }
         cur_slider.position(80, i * 20 + 50);
         cur_slider.style('width', '150px');
         out.push(cur_slider)
         initVal.push(sliderInit[i][2]);
     }
     return [out, initVal];
+}
+
+function checkboxInit(ref, f, p=null) {
+    if (p == null){
+        checkbox = createCheckbox('', true);
+    }
+    else{
+        checkbox = p.createCheckbox('', true);
+    }
+    checkbox.changed(f);
+    checkbox.position(ref.x + ref.width + 120, ref.y + 2);
+    return checkbox;
+}
+
+function drawUI(sliders, sliderTexts, checkbox, realN, colors, p=null){
+    if (p == null){
+        drawUIGlobal(sliders, sliderTexts, checkbox, realN, colors);
+    }
+    else{
+        drawUIInstance(sliders, sliderTexts, checkbox, realN, colors, p);
+    }
+}
+
+function drawUIGlobal(sliders, sliderTexts, checkbox, realN, colors) {
+    fill(0);
+    textSize(14);
+    for (let i = 0; i < sliders.length; i++) {
+        text(sliderTexts[i], sliders[i].x + sliders[i].width + 25, sliders[i].y + 17);
+    }
+    text('Fixed N', checkbox.x + 27, sliders[0].y + 17)
+    textSize(12);
+    text('People', checkbox.x + 73, sliders[0].y + 47)
+    textSize(23);
+    text(Math.round(realN), checkbox.x + 5, sliders[0].y + 47)
+
+    for (let i = 0; i < sliders.length; i++) {
+        fill(colors[i]);
+        circle(sliders[i].x + sliders[i].width + 14, sliders[i].y + 12, 10);
+    }
+}
+
+function drawUIInstance(sliders, sliderTexts, checkbox, realN, colors, p) {
+    p.fill(0);
+    p.textSize(14);
+    for (let i = 0; i < sliders.length; i++) {
+        p.text(sliderTexts[i], sliders[i].x + sliders[i].width + 25, sliders[i].y + 17);
+    }
+    p.text('Fixed N', checkbox.x + 27, sliders[0].y + 17)
+    p.textSize(12);
+    p.text('People', checkbox.x + 73, sliders[0].y + 47)
+    p.textSize(23);
+    p.text(Math.round(realN), checkbox.x + 5, sliders[0].y + 47)
+
+    for (let i = 0; i < sliders.length; i++) {
+        p.fill(colors[i]);
+        p.circle(sliders[i].x + sliders[i].width + 14, sliders[i].y + 12, 10);
+    }
 }
 
 function generatePointsMulti(t, rk, N) {
@@ -23,7 +87,7 @@ function generatePointsMulti(t, rk, N) {
 function addLayers(plt, layerNames, allPoints, colors) {
     for (i = 0; i < layerNames.length; i++) {
         plt.addLayer(layerNames[i], allPoints[i]);
-        plt.getLayer(layerNames[i]).setPointColor(color(...colors[i]));
+        plt.getLayer(layerNames[i]).setPointColor(colors[i]);
     }
 }
 
@@ -41,15 +105,7 @@ function getSliderValues(sliders) {
     return out;
 }
 
-function checkboxInit(ref, f) {
-    checkbox = createCheckbox('', true);
-    checkbox.changed(f);
-    checkbox.position(ref.x + ref.width + 120, ref.y + 2);
-    return checkbox;
-}
-
 function drawPlot(plt) {
-    background(255);
     plt.beginDraw();
     plt.drawBackground();
     plt.drawBox();
@@ -65,22 +121,4 @@ function drawPlot(plt) {
 
 function fixedN() {
     useFixedN = this.checked();
-}
-
-function drawUI(sliders, sliderTexts, checkbox, realN, colors) {
-    fill(0);
-    textSize(14);
-    for (let i = 0; i < sliders.length; i++) {
-        text(sliderTexts[i], sliders[i].x + sliders[i].width + 25, sliders[i].y + 17);
-    }
-    text('Fixed N', checkbox.x + 27, sliders[0].y + 17)
-    textSize(12);
-    text('People', checkbox.x + 73, sliders[0].y + 47)
-    textSize(23);
-    text(Math.round(realN), checkbox.x + 5, sliders[0].y + 47)
-
-    for (let i = 0; i < sliders.length; i++) {
-        fill(...colors[i]);
-        circle(sliders[i].x + sliders[i].width + 14, sliders[i].y + 12, 10);
-    }
 }
